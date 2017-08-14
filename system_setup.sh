57a0ac5edcd16292ce48bd2254734a9388c421e5
Maxo Coin <maxocoin@gmail.com>
2017-08-14 20:26:14 +0100

    Commentswq

---
+++ system_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Installs ansible on the current machine
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
